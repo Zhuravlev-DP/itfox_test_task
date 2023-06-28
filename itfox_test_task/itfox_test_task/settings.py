ad4bf48e709a061ba74697100a5517065a7cc8fc
Zhuravlev Dmitry <Zhyravlev-dmitry97@yandex.ru>
2023-06-29 00:57:47 +0500

    Deploy!

--- itfox_test_task/itfox_test_task/settings.py
+++ itfox_test_task/itfox_test_task/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY', default='unsafe-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv('DEBUG', default=True)
 
 ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
 
