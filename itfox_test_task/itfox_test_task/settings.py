043c71f997ac4d8929b9218966b17b61761c176e
Zhuravlev Dmitry <Zhyravlev-dmitry97@yandex.ru>
2023-06-29 00:27:13 +0500

    Deploy

--- itfox_test_task/itfox_test_task/settings.py
+++ itfox_test_task/itfox_test_task/settings.py
@@ -8,12 +8,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@pkk=)6j*&m5=p-&d+^wx-_oz@5egs913%ssn%8orcsam8vp_n'
+SECRET_KEY = os.getenv('SECRET_KEY', default='unsafe-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
 
 
 # Application definition
