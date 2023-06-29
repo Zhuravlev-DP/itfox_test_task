0230c53e0255f1e0a9cf067ab5865273f3e84cf5
Zhuravlev Dmitry <Zhyravlev-dmitry97@yandex.ru>
2023-06-29 11:23:55 +0500

    Deploy

--- itfox_test_task/itfox_test_task/settings.py
+++ itfox_test_task/itfox_test_task/settings.py
@@ -13,9 +13,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv('SECRET_KEY', default='unsafe-secret-key')
 
-DEBUG = os.getenv('DEBUG', default=True)
+DEBUG = (os.getenv('DEBUG', default='True') == 'True')
 
-ALLOWED_HOSTS = ['158.160.106.232', 'web', 'web:8000']
+ALLOWED_HOSTS = get_list_allowed(
+    os.getenv('ALLOWED_HOSTS', default='localhost')
+)
 
 INSTALLED_APPS = [
     'django.contrib.admin',
